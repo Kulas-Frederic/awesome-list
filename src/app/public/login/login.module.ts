87c9c16043600b8870069fa710a14abd3eb4ae1f
kfjm <kfjmfred@hotmail.com>
2020-03-23 17:19:54 +0100

    add connexion form

--- src/app/public/login/login.module.ts
+++ src/app/public/login/login.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { LoginComponent } from './login/login.component';
 import { LoginRoutingModule } from './login-routing.module';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent, LoginFormComponent],
   imports: [
     SharedModule,
     LoginRoutingModule 
