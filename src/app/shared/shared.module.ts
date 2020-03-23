b772267f6e48026f07d34249614032052d853ba6
kfjm <kfjmfred@hotmail.com>
2020-03-23 16:56:49 +0100

    add register forms

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,19 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgxBootstrapModule } from './modules/ngx-bootstrap.module';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [SidenavComponent],
   imports: [
     CommonModule,
-    NgxBootstrapModule
+    NgxBootstrapModule,
+    ReactiveFormsModule
   ],
   exports: [
     CommonModule,
     NgxBootstrapModule,
-    SidenavComponent 
+    SidenavComponent,
+    ReactiveFormsModule 
   ]
 })
 export class SharedModule { }
